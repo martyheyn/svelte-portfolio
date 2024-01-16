38056d24cf52a2f9f22f334bf24432315449df31
Marty Heyn <marty@techslice.com>
2024-01-16 14:53:07 -0500

    update vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,6 @@
 	// analytics
 	import { dev } from '$app/environment';
 	import { inject } from '@vercel/analytics';
-
 	inject({ mode: dev ? 'development' : 'production' });
 
 	import Up from '$lib/components/up.svelte';
